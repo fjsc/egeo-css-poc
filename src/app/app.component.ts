535a96b908c31e68520043bae07099baf0edd7a0
fjsevilla <fjsevilla@stratio.com>
2020-05-06 13:11:55 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, HostListener, ChangeDetectorRef, ElementRef } from '@angular/core';
+import { StHeaderMenuOption, StLauncherGroup } from '@stratio/egeo';
+import { LAUNCHER_ITEMS, HEADER_MENU } from './app.constants';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'egeo-poc';
+
+  public height: string;
+  public launcherTitle: string = 'Services';
+  public headerMenuSchema: StHeaderMenuOption[] = HEADER_MENU;
+  public launcherItems: StLauncherGroup[] = LAUNCHER_ITEMS;
+  public showLauncherMenu: boolean = false;
+  public showModal = false;
+
+  public rotate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  @ViewChild('launcher', { static: false }) launcherElement: ElementRef;
+
+  constructor(private _cd: ChangeDetectorRef) { }
+
+  onMenuClick(): void {
+    this.showLauncherMenu = !this.showLauncherMenu;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event): void {
+    const expandNewValue: boolean = this.showLauncherMenu && this.launcherElement &&
+      this.launcherElement.nativeElement.contains(event.target);
+
+    if (expandNewValue !== this.showLauncherMenu) {
+      this.showLauncherMenu = expandNewValue;
+    }
+  }
 }
